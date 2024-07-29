52b6bbcc5a8805cd874d1ebb0e982903962fc1b0
zhiblcx <1529088692@qq.com>
2024-07-29 09:12:53 +0800

    chore(server): update acquire static resources

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,13 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { env } from 'node:process';
 import { AppModule } from './app.module';
 import { appConfig } from './config/AppConfig';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix(env.SERVER_PREFIX);
+  app.useStaticAssets('public/', { prefix: '/static' });
+  app.useGlobalPipes(new ValidationPipe());
   const options = new DocumentBuilder()
     .setTitle(appConfig.APP_NAME)
     .setDescription(appConfig.DESCRIPTION)
@@ -16,7 +19,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(process.env.SWAGGER_URL, app, document);
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(env.SERVER_PORT);
 }
 
