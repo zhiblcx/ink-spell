3bf12a12fbe93281b5950d2d56157323ad41348f
zhiblcx <1529088692@qq.com>
2024-10-25 10:00:25 +0800

    fix: antd language

--- apps/client/src/shared/provider/AntdProvider/index.tsx
+++ apps/client/src/shared/provider/AntdProvider/index.tsx
@@ -1,12 +1,16 @@
-import { Theme } from '@/shared/enums'
+import { LanguageEnums, Theme } from '@/shared/enums'
+import { useLanguageStore } from '@/shared/store'
 import { useThemeStore } from '@/shared/store/ThemeStore'
 import { HappyProvider } from '@ant-design/happy-work-theme'
 import { ConfigProvider, theme } from 'antd'
+import English from 'antd/locale/en_US'
+import Chinese from 'antd/locale/zh_CN'
 import { PropsWithChildren } from 'react'
 
 export default function AntdProvider(props: PropsWithChildren) {
   const { children } = props
   const { theme: themeStore } = useThemeStore()
+  const { language } = useLanguageStore()
   return (
     <ConfigProvider
       theme={{
@@ -15,6 +19,7 @@ export default function AntdProvider(props: PropsWithChildren) {
           colorPrimary: themeStore === Theme.DARK ? '#4b4b4b' : '#4c5155'
         }
       }}
+      locale={language === LanguageEnums.Chinese ? Chinese : English}
     >
       <HappyProvider>{children}</HappyProvider>
     </ConfigProvider>
