d0b42b0092b25070c40186e787440db7a2282a85
zhiblcx <1529088692@qq.com>
2024-11-16 07:50:13 +0800

    feat(admin): add MoonIcon and SumIcon

---
+++ apps/admin/src/assets/icons/SumIcon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { DarkModeIconProps } from './types'
+const { width, height } = defineProps<DarkModeIconProps>()
+</script>
+
+<template>
+  <div
+    class="i-mingcute:sun-line w-2em h-2em cursor-pointer"
+    :style="{
+      width,
+      height
+    }"
+  />
+</template>
+
+<style scoped></style>
