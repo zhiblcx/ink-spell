45c4fc45bee110c91c9ec73513f82b05de141fe5
zhiblcx <1529088692@qq.com>
2024-11-16 07:54:06 +0800

    fix(client): add example

--- apps/client/src/shared/utils/ThemeUtils.ts
+++ apps/client/src/shared/utils/ThemeUtils.ts
@@ -2,22 +2,46 @@ import { ThemeEnum } from '../enums'
 export class ThemeUtils {
   static readonly #THEME_KEY = 'ink_spell_theme'
 
+  /**
+   * @example
+   * ```ts
+   * ThemeUtils.getTheme() // => 'dark' | 'light' | null
+   * ```
+   */
   static getTheme(): string | null {
     return localStorage.getItem(this.#THEME_KEY)
   }
 
+  /**
+   * @example
+   * ```ts
+   * ThemeUtils.setTheme(theme) // => 'dark' | 'light'
+   * ```
+   */
   static setTheme(theme: ThemeEnum): void {
     localStorage.setItem(this.#THEME_KEY, theme)
   }
 
+  /**
+  * @example
+  * ```ts
+  * ThemeUtils.clearTheme()
+  * ```
+  */
   static clearTheme(): void {
     localStorage.removeItem(this.#THEME_KEY)
   }
 
+  /**
+  * @example
+  * ```ts
+  * ThemeUtils.changeTheme(theme) // => 'dark' | 'light'
+  * ```
+  */
   static changeTheme(theme: ThemeEnum) {
     if (theme === ThemeEnum.DARK || theme === ThemeEnum.LIGHT) {
       document.documentElement.setAttribute('data-theme', theme)
-      ThemeUtils.setTheme(theme)
+      this.setTheme(theme)
     }
     if (theme === ThemeEnum.DARK) {
       document.documentElement.classList.add(ThemeEnum.DARK)
