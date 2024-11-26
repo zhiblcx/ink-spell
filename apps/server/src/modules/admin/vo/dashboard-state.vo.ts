74c16b3e32331a96d8233bee24b42f04cfecc30f
zhiblcx <1529088692@qq.com>
2024-11-26 20:34:41 +0800

    feat(admin): add dashboard-state vo

---
+++ apps/server/src/modules/admin/vo/dashboard-state.vo.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class DashboardStateVo {
+  @ApiProperty({
+    example: 20,
+    description: '注册用户数量',
+  })
+  userNumber: number;
+
+  @ApiProperty({
+    example: 30,
+    description: "书架数量"
+  })
+  bookshelfNumber: number;
+
+  @ApiProperty({
+    example: 40,
+    description: "书籍数量"
+  })
+  bookNumber: number
+
+  @ApiProperty({
+    example: [{ time: "2024/11/24", bookNumber: 20 }],
+    description: "近七天上传的书籍数量"
+  })
+  bookNumberList: { time: string, bookNumber: number }[]
+}
