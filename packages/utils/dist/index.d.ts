32e47e9bb15424c054317113528b7b281ac93e26
zhiblcx <1529088692@qq.com>
2024-11-28 09:46:34 +0800

    chore(utils): chore TransformTimeUtils

--- packages/utils/dist/index.d.ts
+++ packages/utils/dist/index.d.ts
@@ -2,70 +2,78 @@ import dayjs from 'dayjs';
 
 declare class TransformTimeUtils {
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要获取年份的时间戳，默认为当前时间
+    * @description 获取当前时间戳的年份
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取年份的时间戳，默认为当前时间
     * @return {number} 给定时间戳的年份
     * @example
     * ```ts
     * TransformTimeUtils.getYear()
     * ```
     */
-    static getYear(timer?: Date): number;
+    static getYear(timer?: Date | dayjs.Dayjs): number;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要获取月份的时间戳，默认为当前时间
+    * @description 获取当前时间戳的月份
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取月份的时间戳，默认为当前时间
     * @return {number} 给定时间戳的月份
     * @example
     * ```ts
     * TransformTimeUtils.getMonth()
     * ```
     */
-    static getMonth(timer?: Date): number;
+    static getMonth(timer?: Date | dayjs.Dayjs): number;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要获取日期的时间戳，默认为当前时间
+    * @description 获取当前时间戳的日期
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取日期的时间戳，默认为当前时间
     * @return {number} 给定时间戳的日期
     * @example
     * ```ts
     * TransformTimeUtils.getDate()
     * ```
     */
-    static getDate(timer?: Date): number;
+    static getDate(timer?: Date | dayjs.Dayjs): number;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要获取小时的时间戳，默认为当前时间
+     * @description 获取当前时间戳的小时数
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取小时的时间戳，默认为当前时间
     * @return {number} 给定时间的小时
     * @example
     * ```ts
     * TransformTimeUtils.getHour()
     * ```
     */
-    static getHour(timer?: Date): number;
+    static getHour(timer?: Date | dayjs.Dayjs): number;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要获取分钟的时间戳，默认为当前时间
+    * @description 获取当前时间戳的分钟数
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取分钟的时间戳，默认为当前时间
     * @return {number} 给定时间戳的分钟
     * @example
     * ```ts
     * TransformTimeUtils.getMinute()
     * ```
     */
-    static getMinute(timer?: Date): number;
+    static getMinute(timer?: Date | dayjs.Dayjs): number;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要获取秒数的时间戳，默认为当前时间
+    * @description 获取当前时间戳的秒数
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取秒数的时间戳，默认为当前时间
     * @return {number} 给定时间戳的秒数
     * @example
     * ```ts
     * TransformTimeUtils.getSecond()
     * ```
     */
-    static getSecond(timer?: Date): number;
+    static getSecond(timer?: Date | dayjs.Dayjs): number;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要获取的时间戳，默认为当前时间
+    * @description 获取当前时间戳
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取的时间戳，默认为当前时间
     * @return {number} 给定时间戳
     * @example
     * ```ts
     * TransformTimeUtils.getTimestamp()
     * ```
     */
-    static getTimestamp(timer?: Date): number;
+    static getTimestamp(timer?: Date | dayjs.Dayjs): number;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'YYYY-MM-DD HH:mm:ss'] - 可选参数 表示要格式化的时间格式，默认为'YYYY-MM-DD HH:mm:ss'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -73,9 +81,10 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateYMDHMS()
     * ```
     */
-    static formatDateYMDHMS(timer?: Date, format?: string): string;
+    static formatDateYMDHMS(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'YYYY-MM-DD HH:mm'] - 可选参数 表示要格式化的时间格式，默认为'YYYY-MM-DD HH:mm:ss'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -83,9 +92,10 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateYMDHM()
     * ```
     */
-    static formatDateYMDHM(timer?: Date, format?: string): string;
+    static formatDateYMDHM(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'MM-DD HH:mm'] - 可选参数 表示要格式化的时间格式，默认为'MM-DD HH:mm'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -93,9 +103,10 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateMDHM()
     * ```
     */
-    static formatDateMDHM(timer?: Date, format?: string): string;
+    static formatDateMDHM(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'YYYY-MM-DD'] - 可选参数 表示要格式化的时间格式，默认为'YYYY-MM-DD'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -103,9 +114,10 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateYMD()
     * ```
     */
-    static formatDateYMD(timer?: Date, format?: string): string;
+    static formatDateYMD(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'YYYY-MM'] - 可选参数 表示要格式化的时间格式，默认为'YYYY-MM'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -113,9 +125,10 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateYM()
     * ```
     */
-    static formatDateYM(timer?: Date, format?: string): string;
+    static formatDateYM(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'MM-DD'] - 可选参数 表示要格式化的时间格式，默认为'MM-DD'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -123,9 +136,10 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateMD()
     * ```
     */
-    static formatDateMD(timer?: Date, format?: string): string;
+    static formatDateMD(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'HH:mm:ss'] - 可选参数 表示要格式化的时间格式，默认为'HH:mm:ss'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -133,9 +147,10 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateHMS()
     * ```
     */
-    static formatDateHMS(timer?: Date, format?: string): string;
+    static formatDateHMS(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'HH:mm'] - 可选参数 表示要格式化的时间格式，默认为'HH:mm'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -143,9 +158,10 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateHM()
     * ```
     */
-    static formatDateHM(timer?: Date, format?: string): string;
+    static formatDateHM(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+     * @description 格式化时间戳为指定格式
+    * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
     * @param {string} [format = 'mm:ss'] - 可选参数 表示要格式化的时间格式，默认为'mm:ss'
     * @return {string} 按照指定格式格式化的日期时间字符串
     * @example
@@ -153,52 +169,96 @@ declare class TransformTimeUtils {
     * TransformTimeUtils.formatDateMS()
     * ```
     */
-    static formatDateMS(timer?: Date, format?: string): string;
+    static formatDateMS(timer?: Date | dayjs.Dayjs, format?: string): string;
     /**
-    * @param {Date} timer1 - 第一个时间戳，默认为当前时间
-    * @param {Date} timer2 - 第二个时间戳，默认为当前时间
+     * @description 比较两个时间戳的大小
+    * @param {dayjs.Dayjs | Date} timer1 - 第一个时间戳，默认为当前时间
+    * @param {dayjs.Dayjs | Date} timer2 - 第二个时间戳，默认为当前时间
     * @returns {boolean} - 如果timer1 在 timer2 之前，则返回true，否则返回false
     * @example
     * ```ts
     * TransformTimeUtils.compareTimer()
     * ```
     */
-    static compareTimer(timer1?: Date, timer2?: Date): boolean;
+    static compareTimer(timer1?: Date | dayjs.Dayjs, timer2?: Date | dayjs.Dayjs): boolean;
     /**
-    * @param {Date} timer1 - 第一个时间戳，默认为当前时间
-    * @param {Date} timer2 - 第二个时间戳，默认为当前时间
+     * @description 比较两个时间的差异
+    * @param {dayjs.Dayjs | Date} timer1 - 第一个时间戳，默认为当前时间
+    * @param {dayjs.Dayjs | Date} timer2 - 第二个时间戳，默认为当前时间
     * @param {QUnitType | OpUnitType} - 比较单位，默认为'minute'
     * @returns {boolean} - 返回指定单位中两个日期时间之间的差异
     * @example
     * ```ts
-    * TransformTimeUtils.compareTimerMinute()
+    * TransformTimeUtils.compareTimerDiff()
     * ```
     */
-    static compareTimerMinute(timer1?: Date, timer2?: Date, unit?: dayjs.OpUnitType | dayjs.OpUnitType): number;
+    static compareTimerDiff(timer1?: Date | dayjs.Dayjs, timer2?: Date | dayjs.Dayjs, unit?: dayjs.OpUnitType | dayjs.OpUnitType): number;
     /**
-    * @description 获取当前时间之前日期
+    * @description 获取指定时间之前日期
     * @param {number} n - 要获取的时间单位数量
+    * @param {dayjs.Dayjs | Date} timer - 指定的时间，默认为当前时间
     * @param {dayjs.ManipulateType} [unit] - 要获取的时间单位，默认为'day'
     * @param {string} [format] - 要返回的时间格式，默认为'YYYY-MM-DD'
-    * @returns {string} - 返回当前时间单位之前的指定日期时间字符串
+    * @returns {string} - 返回指定时间单位之前的指定日期时间字符串
     * @example
     * ```ts
     * TransformTimeUtils.getUnitsBefore(2)
     * ```
     */
-    static getUnitsBefore(n: number, unit?: dayjs.ManipulateType, format?: string): string;
+    static getUnitsBefore(n: number, timer?: Date | dayjs.Dayjs, unit?: dayjs.ManipulateType, format?: string): string;
     /**
-    * @description 获取当前时间之后日期
+    * @description 获取指定时间之后日期
     * @param {number} n - 要获取的时间单位数量
+    * @param {dayjs.Dayjs | Date} timer - 指定的时间，默认为当前时间
     * @param {dayjs.ManipulateType} [unit] - 要获取的时间单位，默认为'day'
     * @param {string} [format] - 要返回的时间格式，默认为'YYYY-MM-DD'
-    * @returns {string} - 返回当前时间单位之前的指定日期时间字符串
+    * @returns {string} - 返回指定时间时间单位之前的指定日期时间字符串
     * @example
     * ```ts
     * TransformTimeUtils.getUnitsAfter(2)
     * ```
     */
-    static getUnitsAfter(n: number, unit?: dayjs.ManipulateType, format?: string): string;
+    static getUnitsAfter(n: number, timer?: Date | dayjs.Dayjs, unit?: dayjs.ManipulateType, format?: string): string;
+    /**
+    * @description 获取未来特定天数的特定时间
+    * @param {number} n - 要获取的时间单位数量
+    * @param {dayjs.ManipulateType} unit - 要获取的时间单位，默认为'day'
+    * @param {number} hour - 要获取的小时数
+    * @param {number} minute - 要获取的分钟数，默认为0
+    * @param {number} second - 要获取的秒数，默认为0
+    * @returns {dayjs.Dayjs} - 返回未来特定天数的特定时间
+    * @example
+    * ```ts
+    * TransformTimeUtils.getSpecificTimeInFuture(1,6)
+    * ```
+    */
+    static getSpecificTimeInFuture({ n, unit, hour, minute, second }: {
+        n: number;
+        unit: dayjs.ManipulateType;
+        hour: number;
+        minute: number;
+        second: number;
+    }): dayjs.Dayjs;
+    /**
+    * @description 获取过去特定天数的特定时间
+    * @param {number} n - 要获取的时间单位数量
+    * @param {dayjs.ManipulateType} unit - 要获取的时间单位，默认为'day'
+    * @param {number} hour - 要获取的小时数
+    * @param {number} minute - 要获取的分钟数，默认为0
+    * @param {number} second - 要获取的秒数，默认为0
+    * @returns {dayjs.Dayjs} - 返回过去特定天数的特定时间
+    * @example
+    * ```ts
+    * TransformTimeUtils.getSpecificTimeInPass(1,6)
+    * ```
+    */
+    static getSpecificTimeInPass({ n, unit, hour, minute, second }: {
+        n: number;
+        unit: dayjs.ManipulateType;
+        hour: number;
+        minute: number;
+        second: number;
+    }): dayjs.Dayjs;
 }
 
 export { TransformTimeUtils };
--- packages/utils/src/TransformTimeUtils.ts
+++ packages/utils/src/TransformTimeUtils.ts
@@ -3,91 +3,99 @@ import dayjs from 'dayjs';
 export class TransformTimeUtils {
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要获取年份的时间戳，默认为当前时间
+  * @description 获取当前时间戳的年份
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取年份的时间戳，默认为当前时间
   * @return {number} 给定时间戳的年份
   * @example
   * ```ts
   * TransformTimeUtils.getYear()
   * ```
   */
-  static getYear(timer = new Date()) {
+  static getYear(timer: Date | dayjs.Dayjs = dayjs()): number {
     return dayjs(timer).year()
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要获取月份的时间戳，默认为当前时间
+  * @description 获取当前时间戳的月份
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取月份的时间戳，默认为当前时间
   * @return {number} 给定时间戳的月份
   * @example
   * ```ts
   * TransformTimeUtils.getMonth()
   * ```
   */
-  static getMonth(timer = new Date()) {
+  static getMonth(timer: Date | dayjs.Dayjs = dayjs()): number {
     return dayjs(timer).month() + 1
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要获取日期的时间戳，默认为当前时间
+  * @description 获取当前时间戳的日期
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取日期的时间戳，默认为当前时间
   * @return {number} 给定时间戳的日期
   * @example
   * ```ts
   * TransformTimeUtils.getDate()
   * ```
   */
-  static getDate(timer = new Date()) {
+  static getDate(timer: Date | dayjs.Dayjs = dayjs()): number {
     return dayjs(timer).date()
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要获取小时的时间戳，默认为当前时间
+   * @description 获取当前时间戳的小时数
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取小时的时间戳，默认为当前时间
   * @return {number} 给定时间的小时
   * @example
   * ```ts
   * TransformTimeUtils.getHour()
   * ```
   */
-  static getHour(timer = new Date()) {
+  static getHour(timer: Date | dayjs.Dayjs = dayjs()): number {
     return dayjs(timer).hour()
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要获取分钟的时间戳，默认为当前时间
+  * @description 获取当前时间戳的分钟数
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取分钟的时间戳，默认为当前时间
   * @return {number} 给定时间戳的分钟
   * @example
   * ```ts
   * TransformTimeUtils.getMinute()
   * ```
   */
-  static getMinute(timer = new Date()) {
+  static getMinute(timer: Date | dayjs.Dayjs = dayjs()): number {
     return dayjs(timer).minute()
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要获取秒数的时间戳，默认为当前时间
+  * @description 获取当前时间戳的秒数
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取秒数的时间戳，默认为当前时间
   * @return {number} 给定时间戳的秒数
   * @example
   * ```ts
   * TransformTimeUtils.getSecond()
   * ```
   */
-  static getSecond(timer = new Date()) {
+  static getSecond(timer: Date | dayjs.Dayjs = dayjs()): number {
     return dayjs(timer).second()
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要获取的时间戳，默认为当前时间
+  * @description 获取当前时间戳
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要获取的时间戳，默认为当前时间
   * @return {number} 给定时间戳
   * @example
   * ```ts
   * TransformTimeUtils.getTimestamp()
   * ```
   */
-  static getTimestamp(timer = new Date()) {
+  static getTimestamp(timer: Date | dayjs.Dayjs = dayjs()): number {
     return dayjs(timer).valueOf()
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'YYYY-MM-DD HH:mm:ss'] - 可选参数 表示要格式化的时间格式，默认为'YYYY-MM-DD HH:mm:ss'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -95,12 +103,14 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateYMDHMS()
   * ```
   */
-  static formatDateYMDHMS(timer = new Date(), format = 'YYYY-MM-DD HH:mm:ss') {
+  static formatDateYMDHMS(timer: Date | dayjs.Dayjs = dayjs(), format = 'YYYY-MM-DD HH:mm:ss'): string {
+
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'YYYY-MM-DD HH:mm'] - 可选参数 表示要格式化的时间格式，默认为'YYYY-MM-DD HH:mm:ss'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -108,12 +118,13 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateYMDHM()
   * ```
   */
-  static formatDateYMDHM(timer = new Date(), format = 'YYYY-MM-DD HH:mm') {
+  static formatDateYMDHM(timer: Date | dayjs.Dayjs = dayjs(), format = 'YYYY-MM-DD HH:mm'): string {
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'MM-DD HH:mm'] - 可选参数 表示要格式化的时间格式，默认为'MM-DD HH:mm'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -121,12 +132,13 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateMDHM()
   * ```
   */
-  static formatDateMDHM(timer = new Date(), format = 'MM-DD HH:mm') {
+  static formatDateMDHM(timer: Date | dayjs.Dayjs = dayjs(), format = 'MM-DD HH:mm'): string {
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'YYYY-MM-DD'] - 可选参数 表示要格式化的时间格式，默认为'YYYY-MM-DD'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -134,12 +146,13 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateYMD()
   * ```
   */
-  static formatDateYMD(timer = new Date(), format = 'YYYY-MM-DD') {
+  static formatDateYMD(timer: Date | dayjs.Dayjs = dayjs(), format = 'YYYY-MM-DD'): string {
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'YYYY-MM'] - 可选参数 表示要格式化的时间格式，默认为'YYYY-MM'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -147,12 +160,13 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateYM()
   * ```
   */
-  static formatDateYM(timer = new Date(), format = 'YYYY-MM') {
+  static formatDateYM(timer: Date | dayjs.Dayjs = dayjs(), format = 'YYYY-MM'): string {
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'MM-DD'] - 可选参数 表示要格式化的时间格式，默认为'MM-DD'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -160,12 +174,13 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateMD()
   * ```
   */
-  static formatDateMD(timer = new Date(), format = 'MM-DD') {
+  static formatDateMD(timer: Date | dayjs.Dayjs = dayjs(), format = 'MM-DD'): string {
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'HH:mm:ss'] - 可选参数 表示要格式化的时间格式，默认为'HH:mm:ss'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -173,12 +188,13 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateHMS()
   * ```
   */
-  static formatDateHMS(timer = new Date(), format = 'HH:mm:ss') {
+  static formatDateHMS(timer: Date | dayjs.Dayjs = dayjs(), format = 'HH:mm:ss'): string {
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'HH:mm'] - 可选参数 表示要格式化的时间格式，默认为'HH:mm'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -186,12 +202,13 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateHM()
   * ```
   */
-  static formatDateHM(timer = new Date(), format = 'HH:mm') {
+  static formatDateHM(timer: Date | dayjs.Dayjs = dayjs(), format = 'HH:mm'): string {
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} [timer = new Date()] - 可选参数 表示要格式化的时间戳，默认为当前时间
+   * @description 格式化时间戳为指定格式
+  * @param {dayjs.Dayjs | Date} [timer =  dayjs()] - 可选参数 表示要格式化的时间戳，默认为当前时间
   * @param {string} [format = 'mm:ss'] - 可选参数 表示要格式化的时间格式，默认为'mm:ss'
   * @return {string} 按照指定格式格式化的日期时间字符串
   * @example
@@ -199,67 +216,108 @@ export class TransformTimeUtils {
   * TransformTimeUtils.formatDateMS()
   * ```
   */
-  static formatDateMS(timer = new Date(), format = 'mm:ss') {
+  static formatDateMS(timer: Date | dayjs.Dayjs = dayjs(), format = 'mm:ss'): string {
     return dayjs(timer).format(format)
   }
 
   /**
-  * @param {Date} timer1 - 第一个时间戳，默认为当前时间
-  * @param {Date} timer2 - 第二个时间戳，默认为当前时间
+   * @description 比较两个时间戳的大小
+  * @param {dayjs.Dayjs | Date} timer1 - 第一个时间戳，默认为当前时间
+  * @param {dayjs.Dayjs | Date} timer2 - 第二个时间戳，默认为当前时间
   * @returns {boolean} - 如果timer1 在 timer2 之前，则返回true，否则返回false
   * @example
   * ```ts
   * TransformTimeUtils.compareTimer()
   * ```
   */
-  static compareTimer(timer1 = new Date(), timer2 = new Date()) {
+  static compareTimer(timer1: Date | dayjs.Dayjs = dayjs(), timer2: Date | dayjs.Dayjs = dayjs()): boolean {
     return dayjs(timer1).isBefore(dayjs(timer2))
   }
 
   /**
-  * @param {Date} timer1 - 第一个时间戳，默认为当前时间
-  * @param {Date} timer2 - 第二个时间戳，默认为当前时间
+   * @description 比较两个时间的差异
+  * @param {dayjs.Dayjs | Date} timer1 - 第一个时间戳，默认为当前时间
+  * @param {dayjs.Dayjs | Date} timer2 - 第二个时间戳，默认为当前时间
   * @param {QUnitType | OpUnitType} - 比较单位，默认为'minute'
   * @returns {boolean} - 返回指定单位中两个日期时间之间的差异
   * @example
   * ```ts
-  * TransformTimeUtils.compareTimerMinute()
+  * TransformTimeUtils.compareTimerDiff()
   * ```
   */
-  // 比较两个时间
-  static compareTimerMinute(timer1 = new Date(), timer2 = new Date(), unit?: dayjs.OpUnitType | dayjs.OpUnitType) {
+  static compareTimerDiff(timer1: Date | dayjs.Dayjs = dayjs(), timer2: Date | dayjs.Dayjs = dayjs(), unit?: dayjs.OpUnitType | dayjs.OpUnitType): number {
     if (timer1 < timer2) [timer1, timer2] = [timer2, timer1]
     return dayjs(timer1).diff(dayjs(timer2), unit ?? 'minute')
   }
 
 
   /**
-  * @description 获取当前时间之前日期
+  * @description 获取指定时间之前日期
   * @param {number} n - 要获取的时间单位数量
+  * @param {dayjs.Dayjs | Date} timer - 指定的时间，默认为当前时间
   * @param {dayjs.ManipulateType} [unit] - 要获取的时间单位，默认为'day'
   * @param {string} [format] - 要返回的时间格式，默认为'YYYY-MM-DD'
-  * @returns {string} - 返回当前时间单位之前的指定日期时间字符串
+  * @returns {string} - 返回指定时间单位之前的指定日期时间字符串
   * @example
   * ```ts
   * TransformTimeUtils.getUnitsBefore(2)
   * ```
   */
-  static getUnitsBefore(n: number, unit?: dayjs.ManipulateType, format = 'YYYY-MM-DD') {
-    return dayjs().subtract(n, unit ?? 'day').format(format)
+  static getUnitsBefore(n: number, timer: Date | dayjs.Dayjs = dayjs(), unit: dayjs.ManipulateType = 'day', format = 'YYYY-MM-DD'): string {
+    return dayjs(timer).subtract(n, unit).format(format)
   }
 
   /**
-  * @description 获取当前时间之后日期
+  * @description 获取指定时间之后日期
   * @param {number} n - 要获取的时间单位数量
+  * @param {dayjs.Dayjs | Date} timer - 指定的时间，默认为当前时间
   * @param {dayjs.ManipulateType} [unit] - 要获取的时间单位，默认为'day'
   * @param {string} [format] - 要返回的时间格式，默认为'YYYY-MM-DD'
-  * @returns {string} - 返回当前时间单位之前的指定日期时间字符串
+  * @returns {string} - 返回指定时间时间单位之前的指定日期时间字符串
   * @example
   * ```ts
   * TransformTimeUtils.getUnitsAfter(2)
   * ```
   */
-  static getUnitsAfter(n: number, unit?: dayjs.ManipulateType, format = 'YYYY-MM-DD') {
-    return dayjs().add(n, unit ?? 'day').format(format)
+  static getUnitsAfter(n: number, timer: Date | dayjs.Dayjs = dayjs(), unit: dayjs.ManipulateType = 'day', format = 'YYYY-MM-DD'): string {
+    return dayjs(timer).add(n, unit).format(format)
+  }
+
+  /**
+  * @description 获取未来特定天数的特定时间
+  * @param {number} n - 要获取的时间单位数量
+  * @param {dayjs.ManipulateType} unit - 要获取的时间单位，默认为'day'
+  * @param {number} hour - 要获取的小时数
+  * @param {number} minute - 要获取的分钟数，默认为0
+  * @param {number} second - 要获取的秒数，默认为0
+  * @returns {dayjs.Dayjs} - 返回未来特定天数的特定时间
+  * @example
+  * ```ts
+  * TransformTimeUtils.getSpecificTimeInFuture(1,6)
+  * ```
+  */
+  static getSpecificTimeInFuture({ n, unit = 'day', hour, minute = 0, second = 0 }: {
+    n: number; unit: dayjs.ManipulateType; hour: number; minute: number; second: number
+  }): dayjs.Dayjs {
+    return dayjs().add(n, unit).hour(hour).minute(minute).second(second)
+  }
+
+  /**
+  * @description 获取过去特定天数的特定时间
+  * @param {number} n - 要获取的时间单位数量
+  * @param {dayjs.ManipulateType} unit - 要获取的时间单位，默认为'day'
+  * @param {number} hour - 要获取的小时数
+  * @param {number} minute - 要获取的分钟数，默认为0
+  * @param {number} second - 要获取的秒数，默认为0
+  * @returns {dayjs.Dayjs} - 返回过去特定天数的特定时间
+  * @example
+  * ```ts
+  * TransformTimeUtils.getSpecificTimeInPass(1,6)
+  * ```
+  */
+  static getSpecificTimeInPass({ n, unit = 'day', hour, minute = 0, second = 0 }: {
+    n: number; unit: dayjs.ManipulateType; hour: number; minute: number; second: number
+  }): dayjs.Dayjs {
+    return dayjs().subtract(n, unit).hour(hour).minute(minute).second(second)
   }
 }
