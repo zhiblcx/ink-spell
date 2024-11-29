0d23dba5095652cadc3096c0bc636c01a5582db7
zhiblcx <1529088692@qq.com>
2024-11-29 19:34:28 +0800

    chore

---
+++ apps/admin/src/shared/components/LanguageToggle/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { TranslateIcon } from '@/assets/icons'
+import { LanguageEnum } from '@/shared/enums/LanguageEnum'
+import { useLanguageStore } from '@/shared/store/useLanguageStore'
+const languageStore = useLanguageStore()
+
+const options = [
+  {
+    label: '简体中文',
+    key: LanguageEnum.CHINESE
+  },
+  {
+    label: 'English',
+    key: LanguageEnum.ENGLISH
+  }
+]
+
+function changeTheme(key: LanguageEnum) {
+  languageStore.setLanguage(key)
+}
+</script>
+
+<template>
+  <n-dropdown
+    :options="options"
+    @select="changeTheme"
+  >
+    <TranslateIcon />
+  </n-dropdown>
+</template>
--- apps/admin/src/shared/components/ThemeToggle/index.vue
+++ apps/admin/src/shared/components/ThemeToggle/index.vue
@@ -1,30 +1,44 @@
 <script setup lang="ts">
 import { MoonIcon, SumIcon, SunFlowerIcon } from '@/assets/icons'
 import { ThemeEnum } from '@/shared/enums/ThemeEnum'
+import { useLanguageStore } from '@/shared/store/useLanguageStore'
 import { useThemeStore } from '@/shared/store/useThemeStore'
 import { renderIcon } from '@/shared/utils/renderIcon'
+import { useTranslation } from 'i18next-vue'
+
+const { t } = useTranslation(['COMMON'])
 const themeStore = useThemeStore()
+const languageStore = useLanguageStore()
 const baseIcon = '20px'
+const options = ref()
 
-const options = [
-  {
-    label: '白天模式',
-    key: ThemeEnum.LIGHT,
-    icon: renderIcon(SumIcon, baseIcon, baseIcon)
+watch(
+  () => languageStore.language,
+  () => {
+    options.value = [
+      {
+        label: t('COMMON:night_mode'),
+        key: ThemeEnum.LIGHT,
+        icon: renderIcon(SumIcon, baseIcon, baseIcon)
+      },
+      {
+        label: t('COMMON:day_mode'),
+        key: ThemeEnum.DARK,
+        icon: renderIcon(MoonIcon, baseIcon, baseIcon)
+      },
+      {
+        label: t('COMMON:follow_system'),
+        key: ThemeEnum.SYSTEM,
+        icon: renderIcon(SunFlowerIcon, baseIcon, baseIcon)
+      }
+    ]
   },
   {
-    label: '暗夜模式',
-    key: ThemeEnum.DARK,
-    icon: renderIcon(MoonIcon, baseIcon, baseIcon)
-  },
-  {
-    label: '跟随系统',
-    key: ThemeEnum.SYSTEM,
-    icon: renderIcon(SunFlowerIcon, baseIcon, baseIcon)
+    immediate: true
   }
-]
+)
 
-function changeTheme(key: ThemeEnum) {
+function changeLanguage(key: ThemeEnum) {
   themeStore.changeTheme(key)
 }
 </script>
@@ -32,10 +46,10 @@ function changeTheme(key: ThemeEnum) {
 <template>
   <n-dropdown
     :options="options"
-    @select="changeTheme"
+    @select="changeLanguage"
   >
-    <MoonIcon v-if="themeStore.theme === ThemeEnum.LIGHT" />
-    <SumIcon v-if="themeStore.theme === ThemeEnum.DARK" />
+    <SumIcon v-if="themeStore.theme === ThemeEnum.LIGHT" />
+    <MoonIcon v-if="themeStore.theme === ThemeEnum.DARK" />
     <SunFlowerIcon v-if="themeStore.theme === ThemeEnum.SYSTEM" />
   </n-dropdown>
 </template>
