f66d032c25d610d706c7b941eeb55738b4199ab8
zhiblcx <1529088692@qq.com>
2024-12-01 16:28:49 +0800

    chore(admin): book query

--- apps/admin/src/features/book/query.ts
+++ apps/admin/src/features/book/query.ts
@@ -2,8 +2,8 @@ import { httpRequest } from "@/shared/API";
 import { QueryKeysEnum } from "@/shared/enums/QueryKeysEnum";
 import { useQuery } from "@tanstack/vue-query";
 
-export const selectAllBookQuery = () =>
+export const selectAllBookQuery = (page: Ref<number>, limit: Ref<number>) =>
   useQuery({
-    queryKey: [QueryKeysEnum.ALL_BOOK_KEY],
-    queryFn: () => httpRequest.get("/book/all/info")
+    queryKey: [QueryKeysEnum.ALL_BOOK_KEY, page, limit],
+    queryFn: () => httpRequest.get(`/book/all/info?page=${page.value}&limit=${limit.value}`)
   })
