b189c6268d2b2137d35d5a54da01fe29e0068e22
zhiblcx <1529088692@qq.com>
2024-12-02 11:06:03 +0800

    refactor(admin): PaginationParams

---
+++ apps/admin/src/shared/components/DataTablePagination/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { PaginationParams } from '@/shared/constants'
+import { PaginationType } from '@/shared/types'
+import { DataTableColumn } from 'naive-ui'
+
+interface DataTablePaginationType {
+  columns: Array<DataTableColumn>
+  data: []
+  pageCount: number
+}
+
+const props = defineProps<DataTablePaginationType>()
+const page = defineModel<number>('page', { required: true })
+const pageSize = defineModel<number>('pageSize', { required: true })
+
+const pagination = ref<PaginationType>({
+  page: page.value,
+  pageSize: pageSize.value,
+  pageCount: props.pageCount,
+  pageSizes: PaginationParams.DEFAULT_PAGESIZES,
+  showSizePicker: true,
+  onChange: (currentPage: number) => {
+    page.value = currentPage
+    pagination.value.page = currentPage
+  },
+  onUpdatePageSize: (currentPageSize: number) => {
+    pageSize.value = currentPageSize
+    pagination.value.pageSize = currentPageSize
+  }
+})
+</script>
+
+<template>
+  <n-data-table
+    class="h-[70vh]"
+    :columns="props.columns"
+    :bordered="false"
+    :single-line="false"
+    :pagination="pagination"
+    :max-height="420"
+    striped
+    remote
+    :data="props.data"
+  />
+</template>
--- apps/admin/src/shared/constants/PaginationParams.ts
+++ apps/admin/src/shared/constants/PaginationParams.ts
@@ -1,5 +1,6 @@
 export const PaginationParams = {
   DEFAULT_PAGE: 1,
   DEFAULT_PAGESIZE: 10,
+  DEFAULT_PAGECOUNT: 1,
   DEFAULT_PAGESIZES: [10, 20, 30],
 }
