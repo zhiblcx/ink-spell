2661ccf6db67f5a58a2645d6acc99ada95212677
zhiblcx <1529088692@qq.com>
2024-12-02 11:06:56 +0800

    chore(admin): add DataTablePagination

--- apps/admin/src/view/BookManage.vue
+++ apps/admin/src/view/BookManage.vue
@@ -1,19 +1,90 @@
 <script setup lang="ts">
-import { useLanguageStore } from '@/shared/store/useLanguageStore'
+import { downloadBookByIdMutation, selectAllBookQuery } from '@/features/book'
+import { BookDataVo } from '@/features/book/types'
+import { DataTablePagination } from '@/shared/components'
+import { PaginationParams, SERVER_URL } from '@/shared/constants'
 import { useTranslation } from 'i18next-vue'
-const languageStore = useLanguageStore()
-const { t } = useTranslation(['COMMON', 'VALIDATION'])
-const selectOptions = ref()
+import { DataTableColumn, NAvatar, NButton } from 'naive-ui'
 
-watch(
-  () => languageStore.language,
-  () => {
-    selectOptions.value = [
-      { label: t('COMMON:bookshelf_name'), value: '书架名' },
-      { label: t('COMMON:username'), value: '用户名' }
-    ]
-  },
-  { immediate: true }
+const { t } = useTranslation(['COMMON', 'VALIDATION'])
+const selectOptions = computed(() => [
+  { label: t('COMMON:bookshelf_name'), value: '书架名' },
+  { label: t('COMMON:username'), value: '用户名' }
+])
+const page = ref(PaginationParams.DEFAULT_PAGE)
+const pageSize = ref(PaginationParams.DEFAULT_PAGESIZE)
+const columns = computed(
+  (): Array<DataTableColumn> => [
+    {
+      title: t('COMMON:username'),
+      key: 'username'
+    },
+    {
+      title: t('COMMON:book_name'),
+      key: 'book_name'
+    },
+    {
+      title: t('COMMON:cover'),
+      key: 'cover',
+      align: 'center',
+      render: (rowData) => h(NAvatar, { src: rowData.cover as string, size: 40 })
+    },
+    {
+      title: t('COMMON:author'),
+      key: 'author'
+    },
+    {
+      title: t('COMMON:belonging_bookshelf'),
+      key: 'belonging_bookshelf'
+    },
+    {
+      title: t('COMMON:bookshelf_details'),
+      key: 'bookshelf_details'
+    },
+    {
+      title: t('COMMON:book_introduction'),
+      key: 'book_introduction'
+    },
+    {
+      title: t('COMMON:book_detail'),
+      key: 'book_detail',
+      render: (book) =>
+        h(
+          NButton,
+          { text: true, type: 'primary', onClick: () => downloadMutate(book.id as number) },
+          { default: () => t('COMMON:download') }
+        )
+    },
+    {
+      title: t('COMMON:actions'),
+      key: 'actions',
+      width: '200px',
+      render: () => {
+        const deleteButton = h(NButton, { type: 'error' }, { default: () => t('COMMON:delete') })
+        const updateButton = h(
+          NButton,
+          { style: { marginRight: '10px' }, type: 'primary' },
+          { default: () => t('COMMON:change') }
+        )
+        return [updateButton, deleteButton]
+      }
+    }
+  ]
+)
+const { mutate: downloadMutate } = downloadBookByIdMutation()
+const { data: allBookData } = selectAllBookQuery(page, pageSize)
+
+const processedData = computed(() =>
+  allBookData?.value?.data.items.map((book: BookDataVo) => ({
+    id: book.id,
+    username: book.user.username,
+    book_name: book.name,
+    belonging_bookshelf: book.bookShelf.label,
+    cover: SERVER_URL + book.cover,
+    book_introduction: book.description ?? t('not_available'),
+    author: book.author ?? t('not_available'),
+    bookshelf_details: book.bookShelf.description ?? t('not_available')
+  }))
 )
 </script>
 
@@ -31,33 +102,11 @@ watch(
     <n-button type="primary"> {{ t('COMMON:search') }} </n-button>
   </n-input-group>
 
-  <n-table
-    :bordered="false"
-    :single-line="false"
-  >
-    <thead>
-      <tr>
-        <th>{{ t('COMMON:username') }}</th>
-        <th>{{ t('COMMON:book_name') }}</th>
-        <th>{{ t('COMMON:belonging_bookshelf') }}</th>
-        <th>{{ t('COMMON:book_introduction') }}</th>
-        <th>{{ t('COMMON:book_detail') }}</th>
-        <th>{{ t('COMMON:actions') }}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>放弃</td>
-        <td>1529088692@qq.com</td>
-        <td>1529088692@qq.com</td>
-        <td>1529088692@qq.com</td>
-        <td>1529088692@qq.com</td>
-        <td class="space-x-2">
-          <span>{{ t('COMMON:delete') }}</span>
-        </td>
-      </tr>
-    </tbody>
-  </n-table>
+  <DataTablePagination
+    :columns="columns"
+    :data="processedData"
+    :page-count="allBookData?.data.totalPages"
+    v-model:page="page"
+    v-model:page-size="pageSize"
+  />
 </template>
-
-<style></style>
