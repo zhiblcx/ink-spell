f78b4458deee650f0fc3d0441a6d74dbc36da33e
zhiblcx <1529088692@qq.com>
2024-12-07 08:17:10 +0800

    feat(client): add tag query

---
+++ apps/client/src/features/tag/query.ts
@@ -0,0 +1,7 @@
+import { httpRequest } from "@ink-spell/axios";
+
+export const getAllTagQuery = () =>
+  useQuery({
+    queryKey: [QueryKeysEnum.ALL_TAG],
+    queryFn: () => httpRequest.get('tag')
+  })
