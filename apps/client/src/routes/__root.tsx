c0bbf51bfcdd4e5df603463652155c237419c605
zhiblcx <1529088692@qq.com>
2025-01-13 11:51:53 +0800

    chore(client): __root.tsx login

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -4,6 +4,48 @@ import { SystemConstant } from '@/shared/constants/system'
 import { notification } from 'antd'
 
 export const Route = createRootRoute({
+  beforeLoad: (router) => {
+    if (router.location.href !== '/signin' && router.location.href !== '/signup') {
+      Page = () => {
+        const [api, contextHolder] = notification.useNotification()
+        const { t } = useTranslation(['COMMON'])
+        const { data: announcementUserData, isLoading } = getAnnouncementUserQuery()
+        const { mutate: updateAnnouncementUserMutate } = updateAnnouncementUserMutation()
+
+        const openNotification = () => {
+          const key = `open${Date.now()}`
+          const btn = (
+            <Space>
+              <Button
+                type="primary"
+                size="small"
+                onClick={() => api.destroy(key)}
+              >
+                {t('COMMON:knew')}
+              </Button>
+            </Space>
+          )
+          api.open({
+            message: t('COMMON:announcement'),
+            description: announcementUserData?.data.text,
+            btn,
+            key,
+            onClose: () => {
+              updateAnnouncementUserMutate(announcementUserData?.data.id)
+            }
+          })
+        }
+
+        useEffect(() => {
+          if (!isLoading && announcementUserData?.data.status == SystemConstant.status.UNREAD) {
+            openNotification()
+          }
+        }, [isLoading])
+
+        return <>{contextHolder}</>
+      }
+    }
+  },
   component: () => (
     <>
       <Page />
@@ -11,41 +53,5 @@ export const Route = createRootRoute({
     </>
   )
 })
-export function Page() {
-  const [api, contextHolder] = notification.useNotification()
-  const { t } = useTranslation(['COMMON'])
-  const { data: announcementUserData, isLoading } = getAnnouncementUserQuery()
-  const { mutate: updateAnnouncementUserMutate } = updateAnnouncementUserMutation()
 
-  const openNotification = () => {
-    const key = `open${Date.now()}`
-    const btn = (
-      <Space>
-        <Button
-          type="primary"
-          size="small"
-          onClick={() => api.destroy(key)}
-        >
-          {t('COMMON:knew')}
-        </Button>
-      </Space>
-    )
-    api.open({
-      message: t('COMMON:announcement'),
-      description: announcementUserData?.data.text,
-      btn,
-      key,
-      onClose: () => {
-        updateAnnouncementUserMutate(announcementUserData?.data.id)
-      }
-    })
-  }
-
-  useEffect(() => {
-    if (!isLoading && announcementUserData?.data.status == SystemConstant.status.UNREAD) {
-      openNotification()
-    }
-  }, [isLoading])
-
-  return <>{contextHolder}</>
-}
+let Page = () => <></>
