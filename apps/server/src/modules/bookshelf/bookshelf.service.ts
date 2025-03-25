887cffe103ab0ea76b5d4050ae96bb3adc930071
zhiblcx <1529088692@qq.com>
2025-03-25 12:35:01 +0800

    feat(server): add bookshelf review

--- apps/server/src/modules/bookshelf/bookshelf.service.ts
+++ apps/server/src/modules/bookshelf/bookshelf.service.ts
@@ -6,8 +6,8 @@ import { TranslationService } from '../translation/translation.service';
 export class BookshelfService {
   constructor(
     private readonly prisma: PrismaService,
-    private readonly translation: TranslationService
-  ) { }
+    private readonly translation: TranslationService,
+  ) {}
   async createBookShelf(req, createBookshelfDto) {
     const currentBookShelf = await this.prisma.bookShelf.findFirst({
       where: {
@@ -19,12 +19,12 @@ export class BookshelfService {
     if (currentBookShelf) {
       return undefined;
     }
-    const tags = []
+    const tags = [];
     for (let i = 0; i < createBookshelfDto.tags?.length; i++) {
       const tag = await this.prisma.tag.findUnique({
-        where: { id: createBookshelfDto.tags[i] }
-      })
-      tags.push(tag)
+        where: { id: createBookshelfDto.tags[i] },
+      });
+      tags.push(tag);
     }
     const result = await this.prisma.bookShelf.create({
       data: {
@@ -49,9 +49,9 @@ export class BookshelfService {
 
     await this.prisma.bookShelf.update({
       where: { id: result.id },
-      data: { tags: { set: tags ?? null } }
-    })
-    return result
+      data: { tags: { set: tags ?? null } },
+    });
+    return result;
   }
 
   async deleteBookShelf(bookShelfId) {
@@ -79,10 +79,10 @@ export class BookshelfService {
       include: {
         tags: {
           where: {
-            isDelete: false
-          }
-        }
-      }
+            isDelete: false,
+          },
+        },
+      },
     });
   }
 
@@ -93,12 +93,12 @@ export class BookshelfService {
   }
 
   async updateBookShelf(bookShelfId, updateBookshelfDto) {
-    const tags = []
+    const tags = [];
     for (let i = 0; i < updateBookshelfDto.tags?.length; i++) {
       const tag = await this.prisma.tag.findUnique({
-        where: { id: updateBookshelfDto.tags[i] }
-      })
-      tags.push(tag)
+        where: { id: updateBookshelfDto.tags[i] },
+      });
+      tags.push(tag);
     }
     await this.prisma.bookShelf.update({
       data: {
@@ -108,8 +108,9 @@ export class BookshelfService {
           updateBookshelfDto.bookShelfCover ??
           appConfig.DEFAULT_BOOK_SHELF_COVER,
         isPublic: updateBookshelfDto.status,
+        review: updateBookshelfDto.status ? 'PENDING' : 'UNREVIEWED',
         description: updateBookshelfDto.bookShelfDescription,
-        tags: { set: tags ?? null }
+        tags: { set: tags ?? null },
       },
       where: { id: parseInt(bookShelfId) },
     });
@@ -119,54 +120,66 @@ export class BookshelfService {
     page: number,
     limit: number,
     bookshelfName?: string,
-    tagsId?: string
+    tagsId?: string,
   ) {
     return await this.prisma.bookShelf.findMany({
       where: {
         label: { contains: bookshelfName },
-        tags: (tagsId === undefined) ? {} : {
-          some: {
-            id: { in: tagsId?.split(',').map(tagId => (parseInt(tagId))) }
-          }
-        },
+        tags:
+          tagsId === undefined
+            ? {}
+            : {
+                some: {
+                  id: {
+                    in: tagsId?.split(',').map((tagId) => parseInt(tagId)),
+                  },
+                },
+              },
         isDelete: false,
-        isPublic: true
+        isPublic: true,
+        review: 'APPROVED',
       },
       include: {
         tags: {
           where: {
-            isDelete: false
-          }
-        }, user: true
+            isDelete: false,
+          },
+        },
+        user: true,
       },
       skip: (page - 1) * limit,
       take: limit,
-    })
+    });
   }
 
   async getPublicBookShelfCount(
     limit: number,
     bookshelfName?: string,
-    tagsId?: string
+    tagsId?: string,
   ) {
-    return Math.ceil(await this.prisma.bookShelf.count({
-      where: {
-        label: { contains: bookshelfName },
-        tags: (tagsId === undefined) ? {} : {
-          some: {
-            id: { in: tagsId?.split(',').map(tagId => (parseInt(tagId))) }
-          }
+    return Math.ceil(
+      (await this.prisma.bookShelf.count({
+        where: {
+          label: { contains: bookshelfName },
+          tags:
+            tagsId === undefined
+              ? {}
+              : {
+                  some: {
+                    id: {
+                      in: tagsId?.split(',').map((tagId) => parseInt(tagId)),
+                    },
+                  },
+                },
+          isDelete: false,
+          isPublic: true,
+          review: 'APPROVED',
         },
-        isDelete: false,
-        isPublic: true
-      },
-    }) / limit)
+      })) / limit,
+    );
   }
 
-
-
   async getRecommendBookshelf(userId: number) {
-
     // 首先找到用户的喜爱的三个标签
     const collectBookshelf = await this.prisma.collectBookShelf.findMany({
       where: {
@@ -176,23 +189,26 @@ export class BookshelfService {
       include: {
         bookShelf: {
           include: {
-            tags: true
-          }
-        }
-      }
-    })
-    const tags = collectBookshelf.map(item => item.bookShelf.tags).flat().map(item => item.id)
+            tags: true,
+          },
+        },
+      },
+    });
+    const tags = collectBookshelf
+      .map((item) => item.bookShelf.tags)
+      .flat()
+      .map((item) => item.id);
 
     const idCounts = tags.reduce((acc, id) => {
-      acc[id] = (acc[id] || 0) + 1
-      return acc
-    }, {})
+      acc[id] = (acc[id] || 0) + 1;
+      return acc;
+    }, {});
 
     // 将id和它们出现的次数转换为一个数组
-    const idCountsArray = Object.entries(idCounts)
+    const idCountsArray = Object.entries(idCounts);
 
     // 按照出现次数降序排序
-    idCountsArray.sort((a, b) => Number(b[1]) - Number(a[1]))
+    idCountsArray.sort((a, b) => Number(b[1]) - Number(a[1]));
 
     // 选出出现次数最多的三个id
     const topThreeIds = idCountsArray.slice(0, 3).map(([id]) => id);
@@ -201,76 +217,166 @@ export class BookshelfService {
       where: {
         tags: {
           some: {
-            id: { in: topThreeIds.map(id => parseInt(id)) }
-          }
+            id: { in: topThreeIds.map((id) => parseInt(id)) },
+          },
         },
         isDelete: false,
-        isPublic: true
+        isPublic: true,
       },
       include: {
         tags: {
           where: {
-            isDelete: false
-          }
+            isDelete: false,
+          },
         },
-        user: true
+        user: true,
       },
       take: 50,
-    })
+    });
   }
 
-
-  async getAllBookInfo(page: number, limit: number, username: string | undefined, bookshelfName: string | undefined) {
+  async getAllBookInfo(
+    page: number,
+    limit: number,
+    username: string | undefined,
+    bookshelfName: string | undefined,
+  ) {
     const bookShelfs = await this.prisma.bookShelf.findMany({
       where: {
-        label: { contains: bookshelfName ?? '', },
-        user: { username: { contains: username ?? '', } },
-        isDelete: false
+        label: { contains: bookshelfName ?? '' },
+        user: { username: { contains: username ?? '' } },
+        isDelete: false,
       },
       include: {
         user: {
-          select: { id: true, username: true, account: true, email: true, avatar: true }
+          select: {
+            id: true,
+            username: true,
+            account: true,
+            email: true,
+            avatar: true,
+          },
         },
         tags: {
           where: {
-            isDelete: false
-          }
+            isDelete: false,
+          },
         },
         _count: {
           select: {
             collectBookShelf: {
-              where: { isDelete: false }
+              where: { isDelete: false },
             },
-            books: { where: { isDelete: false } }
-          }
-        }
+            books: { where: { isDelete: false } },
+          },
+        },
       },
       skip: (page - 1) * limit,
       take: limit,
     });
 
-    return bookShelfs.map(bookshelf => ({
+    return bookShelfs.map((bookshelf) => ({
       ...bookshelf,
       _count: undefined,
       collectBookShelfPeople: bookshelf._count.collectBookShelf,
       bookCount: bookshelf._count.books,
-    }))
-
+    }));
   }
 
-  async getAllBookInfoCount(limit: number, username: string | undefined, bookshelfName: string | undefined) {
-    return Math.ceil(await this.prisma.bookShelf.count({
+  async getAllBookInfoCount(
+    limit: number,
+    username: string | undefined,
+    bookshelfName: string | undefined,
+  ) {
+    return Math.ceil(
+      (await this.prisma.bookShelf.count({
+        where: {
+          label: {
+            contains: bookshelfName ?? '',
+          },
+          user: {
+            username: {
+              contains: username ?? '',
+            },
+          },
+          isDelete: false,
+        },
+      })) / limit,
+    );
+  }
+
+  async getReviewBookshelf(page: number, limit: number) {
+    return await this.prisma.bookShelf.findMany({
       where: {
-        label: {
-          contains: bookshelfName ?? '',
+        isDelete: false,
+        isPublic: true,
+        review: 'PENDING',
+      },
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+  }
+
+  async getReviewBookshelfCount(limit: number) {
+    return Math.ceil(
+      (await this.prisma.bookShelf.count({
+        where: {
+          isDelete: false,
+          isPublic: true,
+          review: 'PENDING',
         },
-        user: {
-          username: {
-            contains: username ?? '',
-          }
-        },
-        isDelete: false
-      }
-    }) / limit)
+      })) / limit,
+    );
+  }
+
+  async updateReviewBookshelf(bookShelfId: number, review: ReviewStatus) {
+    return await this.prisma.bookShelf.updateMany({
+      where: {
+        id: bookShelfId,
+      },
+      data: {
+        review: review,
+      },
+    });
+  }
+
+  async putReviewApplyBookshelf(userId: number, bookshelfId: number) {
+    return await this.prisma.bookShelf.update({
+      where: {
+        id: bookshelfId,
+      },
+      data: {
+        review: 'PENDING',
+        userId: userId,
+      },
+    });
+  }
+
+  async getReviewPublicBookshelf(userId: number) {
+    // 已拒绝
+    const reject = await this.prisma.bookShelf.findMany({
+      where: {
+        userId: userId,
+        review: 'REJECTED',
+      },
+    });
+
+    // 审核中
+    const pending = await this.prisma.bookShelf.findMany({
+      where: {
+        userId: userId,
+        review: 'PENDING',
+      },
+    });
+
+    // 已通过
+    const approved = await this.prisma.bookShelf.findMany({
+      where: {
+        userId: userId,
+        review: 'APPROVED',
+      },
+    });
+
+    return [...pending, ...reject, ...approved];
   }
 }
